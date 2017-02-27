7b3768db2efa461c8a975f9507b551e83a9ffcc6
T_MaheshP2 <T_MaheshP2@verifone.com>
2017-02-27 18:00:54 +0530

    apache

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -4,6 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-package "httpd" do
+package "httpd.x86_64" do
   action :install
+end
+
+service httpd do
+  action :enable :start
+end
+
+file '/var/www/html/index.html' do
+ content '<html>This is a placeholder for the home page.</html>'
 end
